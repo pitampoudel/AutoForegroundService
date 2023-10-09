07491b00cc8f4b48f9bf6e6d3711383c1a90a0d8
Pitam <pitampoudelsaipu@gmail.com>
2023-10-09 21:18:49 +0545

    java versions

--- autoforegroundservice/build.gradle.kts
+++ autoforegroundservice/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "pitam.autoforegroundservice"
+
     compileSdk = 33
 
     defaultConfig {
@@ -24,19 +25,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -45,5 +44,5 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     //LifecycleService
-    api("androidx.lifecycle:lifecycle-service:2.5.1")
+    api("androidx.lifecycle:lifecycle-service:2.6.2")
 }
--- autoforegroundservice/src/main/java/pitam/autoforegroundservice/NotificationUtils.kt
+++ autoforegroundservice/src/main/java/pitam/autoforegroundservice/NotificationUtils.kt
@@ -3,9 +3,9 @@ package pitam.autoforegroundservice
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context.NOTIFICATION_SERVICE
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import androidx.lifecycle.LifecycleService
 
 
 object NotificationUtils {
@@ -19,8 +19,7 @@ object NotificationUtils {
         notificationText: String,
         customize: NotificationCompat.Builder.() -> NotificationCompat.Builder = { this }
     ): Notification {
-        val notificationManager =
-            getSystemService(LifecycleService.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
