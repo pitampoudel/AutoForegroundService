7705683f420a00660122b219074247a5b9324e16
Pitam <pitampoudelsaipu@gmail.com>
2024-08-04 11:55:56 +0545

    maven publishing

--- autoforegroundservice/build.gradle.kts
+++ autoforegroundservice/build.gradle.kts
@@ -4,6 +4,17 @@ plugins {
     id("maven-publish")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.findByName("release"))
+            groupId = "pitam"
+            artifactId = "autoforegroundservice"
+            version = "1.0.0"
+        }
+    }
+}
+
 android {
     namespace = "pitam.autoforegroundservice"
 
